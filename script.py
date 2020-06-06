ed401cf6a11266d4c8583cfb8d4de1847e1ed09b
nw <nico.walters@techtalents.es>
2020-06-06 17:13:29 +0200

    Convert puzzle function

--- script.py
+++ script.py
@@ -10,7 +10,25 @@ def read_puzzles():
 
 
 # Parse/convert a puzzle string into a useable data-structure
-
+def convert_puzzle(string):
+    puzzle = []
+    for i in range(9):
+        row = []
+        for j in range(i * 9, (i + 1) * 9):
+            n = string[j]
+            row.append(n)
+        puzzle.append(row)
+    return puzzle
 
 
 # Visualisation of the puzzle
+
+
+
+
+if __name__ == "__main__":
+    puzzles = read_puzzles()
+    puzzle = puzzles[42]
+
+    print(puzzle)
+    print(convert_puzzle(puzzle))
