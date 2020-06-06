4a0cc44cd1a9aa2d8cdefb9db9714f4b4cf39108
nw <nico.walters@techtalents.es>
2020-06-06 17:04:50 +0200

    Read puzzle file

--- script.py
+++ script.py
@@ -3,7 +3,10 @@
 
 
 # Read the file of puzzles
-
+def read_puzzles():
+    with open("puzzles") as file:
+        content = file.read().splitlines()
+        return content
 
 
 # Parse/convert a puzzle string into a useable data-structure
